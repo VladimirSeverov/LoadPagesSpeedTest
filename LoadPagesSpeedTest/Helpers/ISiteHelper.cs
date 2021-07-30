dc06627730f6e916342451fa48c6987b3317ed8c
Vladimir Severov <vladimirs0812@gmail.com>
2021-07-30 04:00:46 +0300

    Fix

--- LoadPagesSpeedTest/Helpers/ISiteHelper.cs
+++ LoadPagesSpeedTest/Helpers/ISiteHelper.cs
@@ -7,11 +7,6 @@ namespace LoadPagesSpeedTest.Helpers
     public interface ISiteHelper
     {
         List<string> GetSiteMap(string mainUrl);
-        //string DownloadString(string url);
-        //byte[] DownloadBytes(string url);
-        //int GetDownloadSpeed(string url);
-
-        //Task<List<int>> GetTestResult(string url);
         Task<TestDetails> GetResultBySitemapItem(string url, int id);
     }
 }
